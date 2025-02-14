abd99a7d4935bcdc73261941869566c03f999b13
henriqk0 <henriquedeslima2811@gmail.com>
2025-02-14 20:55:19 -0300

    remove a unused import

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
         Sistema sist = new Sistema();
