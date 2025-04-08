77fb60cac93ebf8ff341648265ac095a4257f1a4
henriqk0 <171154572+henriqk0@users.noreply.github.com>
2025-04-08 12:29:02 -0300

    starting code the reader

--- adv-prog-t/linked-list-comparator/Application.java
+++ adv-prog-t/linked-list-comparator/Application.java
@@ -2,19 +2,21 @@ import linkedlists.LinkedListComparator;
 import student.Student;
 import student.StudentNameComparator;
 import student.StudentRegistrationComparator;
+import filegenerators.LeitorArquivos; 
 
 
 public class Application {
     public static void main(String[] args) {
-        LinkedListComparator<Student> nameOrdList = new LinkedListComparator<>(true, new StudentNameComparator());
-        LinkedListComparator<Student> regOrdList = new LinkedListComparator<>(true, new StudentRegistrationComparator());
-        Student s1 = new Student(1, "Iohannes", 50);
-        Student s2 = new Student(2, "Ambrosius", 58);
-        nameOrdList.insertElemOrdered(s1);
-        nameOrdList.insertElemOrdered(s2);
-        regOrdList.insertElemOrdered(s1);
-        regOrdList.insertElemOrdered(s2);
-        System.out.println("Ordered list by name: " + nameOrdList);
+        // LinkedListComparator<Student> nameOrdList = new LinkedListComparator<>(true, new StudentNameComparator());
+        
+        LinkedListComparator<Student> regOrdList = LeitorArquivos.populateListOrdered();
+        // Student s1 = new Student(1, "Iohannes", 50);
+        // Student s2 = new Student(2, "Ambrosius", 58);
+        // nameOrdList.insertElemOrdered(s1);
+        // nameOrdList.insertElemOrdered(s2);
+        // regOrdList.insertElemOrdered(s1);
+        // regOrdList.insertElemOrdered(s2);
+        // System.out.println("Ordered list by name: " + nameOrdList);
         System.out.println("Ordered list by registration: " + regOrdList);
     }
 
--- adv-prog-t/linked-list-comparator/filegenerators/LeitorArquivos.java
+++ adv-prog-t/linked-list-comparator/filegenerators/LeitorArquivos.java
@@ -4,11 +4,67 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import linkedlists.LinkedListComparator;
+import linkedlists.LinkedListNoOrdered;
+import student.Student;
+import student.StudentRegistrationComparator;
+
 
 public class LeitorArquivos {
     private static final String NOME_ARQUIVO = "alunosOrdenados.txt";
     
+    public static LinkedListComparator<Student> populateListOrdered() {
+        LinkedListComparator<Student> regOrdList = new LinkedListComparator<>(true, new StudentRegistrationComparator());
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(NOME_ARQUIVO))) {
+            int numRegistros = Integer.parseInt(reader.readLine().trim());
+            System.out.println("Número de registros: " + numRegistros);
+            
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                String[] partes = linha.split(";");
+                int id = Integer.parseInt(partes[0]);
+                String nome = partes[1];
+                float nota = Float.parseFloat(partes[2]);
+                
+                Student s = new Student(id, nome, nota);
+                regOrdList.insertElemOrdered(s);
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Erro ao processar um dos valores numéricos: " + e.getMessage());
+        }
+        return regOrdList;
+    }
+
+    public LinkedListNoOrdered<Student> populateListNoOrdered() {
+        LinkedListNoOrdered<Student> noOrdList = new LinkedListNoOrdered<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(NOME_ARQUIVO))) {
+            int numRegistros = Integer.parseInt(reader.readLine().trim());
+            System.out.println("Número de registros: " + numRegistros);
+            
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                String[] partes = linha.split(";");
+                int id = Integer.parseInt(partes[0]);
+                String nome = partes[1];
+                float nota = Float.parseFloat(partes[2]);
+                
+                Student s = new Student(id, nome, nota);
+                noOrdList.insertElem(s);
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Erro ao processar um dos valores numéricos: " + e.getMessage());
+        }
+        return noOrdList;
+    }    
+
     public static void main(String[] args) {
+        
+
         try (BufferedReader reader = new BufferedReader(new FileReader(NOME_ARQUIVO))) {
             int numRegistros = Integer.parseInt(reader.readLine().trim());
             System.out.println("Número de registros: " + numRegistros);
