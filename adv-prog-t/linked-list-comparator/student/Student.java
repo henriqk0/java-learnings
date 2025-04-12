2fe53d442e42caa7a5bd1a9f48698831dc100f5b
Gabriel Barbosa Lucas <gblucas180902@gmail.com>
2025-04-11 21:37:48 -0300

    Menu de interface + padrão singleton

--- adv-prog-t/linked-list-comparator/student/Student.java
+++ adv-prog-t/linked-list-comparator/student/Student.java
@@ -1,22 +1,50 @@
 package student;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+import java.util.Scanner;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Student {
-    public int reg;
-    public String name;
-    public float score;
+    private int reg;
+    private String name;
+    private float score;
 
-    public Student(int reg, String name, float score) {
+    private Student instance = null;
+
+    private Student(int reg, String name, float score) {
         this.reg = reg;
         this.name = name;
         this.score = score;
     }
 
+    private Student(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Informe o nº do aluno:\n");
+        this.reg = s.nextBigInteger().intValue();
+        System.out.println("Informe o nome do aluno:\n");
+        this.name = s.nextLine();
+    }
+
+    public Student getInstance(){
+        if(this.instance == null){
+            this.instance = new Student();
+        }
+        return this.instance;
+    }
+
+    /*
     @Override
     public String toString() {
         return this.reg + " - " + this.name;
-    }
+    }*/
 
+    /*
     @Override
     public boolean equals(Object s) {
         if (s instanceof Student) {
@@ -25,29 +53,5 @@ public class Student {
         else {
             return false;
         }
-    }
-
-    public int getReg() {
-        return reg;
-    }
-
-    public void setReg(int reg) {
-        this.reg = reg;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getScore() {
-        return score;
-    }
-
-    public void setScore(float score) {
-        this.score = score;
-    }
+    }*/
 }
