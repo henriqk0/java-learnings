da8bc1664f130120665fa6e89a811a6c0d83c944
guilhermbc <guilhermeborgescabral@gmail.com>
2025-04-29 10:56:49 -0300

    implementação dos métodos de adicionar e pesquisar

--- adv-prog-t/trees-java/src/lib/ArvoreBinaria.java
+++ adv-prog-t/trees-java/src/lib/ArvoreBinaria.java
@@ -12,32 +12,74 @@ import java.util.Comparator;
  *
  * @author victoriocarvalho
  */
-public class ArvoreBinariaExemplo<T> implements IArvoreBinaria<T> {
+public class ArvoreBinaria<T> implements IArvoreBinaria<T> {
     
-    protected NoExemplo<T> raiz = null;
+    protected No<T> raiz = null;
     protected Comparator<T> comparador; 
   
-    public ArvoreBinariaExemplo(Comparator<T> comp) {
+    public ArvoreBinaria(Comparator<T> comp) {
         comparador = comp;
     }
     
     @Override
     public void adicionar(T novoValor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        No<T> atual = this.raiz;
+
+        while (atual != null){
+            //Se o valor for igual, também vai para a esquerda
+            if (comparador.compare(novoValor, atual.getValor()) <= 0) {
+                atual = atual.getFilhoEsquerda();
+            }
+            else{
+                atual = atual.getFilhoDireita();
+            }
+        }
+
+        atual = new No<T>(novoValor);
     }
 
     @Override
     public T pesquisar(T valor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return pesquisarRecursivo(this.raiz, valor);
     }
 
-   @Override
-    public T pesquisar(T valor, Comparator comparador) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private T pesquisarRecursivo(No<T> raiz, T valor){
+        if (raiz == null || (comparador.compare(raiz.getValor(), valor) == 0)){
+            return raiz.getValor();
+        }
+
+        if (comparador.compare(valor, raiz.getValor()) <= 0) return pesquisarRecursivo(raiz.getFilhoEsquerda(), valor);
+        return pesquisarRecursivo(raiz.getFilhoDireita(), valor);
+    }
+
+    @Override
+    public T pesquisar(T valor, Comparator<T> comparador) {
+        return pesquisarRecursivo(this.raiz, valor, comparador);
+    }
+
+    private T pesquisarRecursivo(No<T> raiz, T valor, Comparator<T> comparador){
+        if (raiz == null) {
+            return null;
+        }
+        else if (comparador.compare(raiz.getValor(), valor) == 0){
+            return raiz.getValor();
+        }
+
+        T returnEsq = pesquisarRecursivo(raiz.getFilhoEsquerda(), valor, comparador);
+        if (returnEsq != null){
+            return returnEsq;
+        }
+        return pesquisarRecursivo(raiz.getFilhoDireita(), valor, comparador);
     }
 
     @Override
     public T remover(T valor) {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private T removerRecursivo(No<T> raiz, T valor) {
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
