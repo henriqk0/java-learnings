f67af5d3e1007ae1238e7863104a4404e21b8bda
henriqk0 <henriquedeslima2811@gmail.com>
2025-05-13 10:33:53 -0300

    comecando arvoreAVL

--- adv-prog-t/trees-java/src/main/java/com/lib/ArvoreAVLExemplo.java
+++ adv-prog-t/trees-java/src/main/java/com/lib/ArvoreAVLExemplo.java
@@ -7,7 +7,25 @@ public class ArvoreAVLExemplo <T> extends ArvoreBinaria<T>{
     public ArvoreAVLExemplo(Comparator<T> comparator) {
         super(comparator);
     }
+
+    @Override
+    public void adicionar(No<T> novoValor) { 
+       
+
+    }
     
     //Implementar métodos para efetuar o balanceamento e sobrescrever método de adicionar elemento...
+    public T rotacaoDireita(No<T> rotac) {
+        No<T> auxNo = rotac.getFilhoEsquerda();
+        auxNo.setFilhoDireita(rotac);
+    }
+
+    public int getAlturaSubarvore(No<T> no) {
+        ArvoreBinaria<T> subArvore = new ArvoreBinaria<>();
+        subArvore.raiz = no;
+
+        return subArvore.altura(); 
+    }
+
 
 }
