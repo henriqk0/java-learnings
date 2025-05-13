ccc5f9ca351e609a2df95560d56ef1782682b772
henriqk0 <henriquedeslima2811@gmail.com>
2025-05-13 11:07:00 -0300

    continuando construcao da avl

--- adv-prog-t/trees-java/src/main/java/com/lib/ArvoreAVLExemplo.java
+++ adv-prog-t/trees-java/src/main/java/com/lib/ArvoreAVLExemplo.java
@@ -8,10 +8,35 @@ public class ArvoreAVLExemplo <T> extends ArvoreBinaria<T>{
         super(comparator);
     }
 
-    @Override
-    public void adicionar(No<T> novoValor) { 
-       
+    public No<T> adicionarRecursivo(No<T> raiz, No<T> novoNo){
+        if (raiz == NULL) {
+            return no;
+        }
 
+        int heRaiz = getAlturaSubarvore(raiz.getFilhoEsquerda());
+        int hdRaiz = getAlturaSubarvore(raiz.getFilhoDireita());
+        int fbRaiz = hdRaiz - heRaiz;
+        
+        int heFilhoDireita = getAlturaSubarvore(raiz.getFilhoDireita().getFilhoEsquerda());
+        int hdFilhoDireita = getAlturaSubarvore(raiz.getFilhoDireita().getFilhoDireita());
+        int fbFilhoDireita = hdFilhoDireita - heFilhoDireita;
+
+        int heFilhoEsquerda = getAlturaSubarvore(raiz.getFilhoEsquerda().getFilhoEsquerda());
+        int hdFilhoEsquerda = getAlturaSubarvore(raiz.getFilhoEsquerda().getFilhoDireita());
+        int fbFilhoEsquerda = hdFilhoEsquerda - heFilhoEsquerda;
+
+        // falta verificações dos fb's e ver se os metodos estao funcionando
+        if (comparator.compare(novoNo.getValor(), raiz.getValor()) < 0) {
+            raiz.setFilhoEsquerda(adicionarRecursivo(raiz.getFilhoEsquerda(), novoNo));
+        } else {
+            raiz.setFilhoEsquerda(adicionarRecursivo(raiz.getFilhoEsquerda(), novoNo));
+        }
+    }
+
+    @Override
+    public void adicionar(T valor) { 
+        No<T> novoNo = new No<T>(valor)
+        ArvoreAVLExemplo.adicionarRecursivo(this.raiz, novoNo);       
     }
     
     //Implementar métodos para efetuar o balanceamento e sobrescrever método de adicionar elemento...
@@ -21,6 +46,10 @@ public class ArvoreAVLExemplo <T> extends ArvoreBinaria<T>{
     }
 
     public int getAlturaSubarvore(No<T> no) {
+        if (no == null) {
+            return 0;
+        }
+
         ArvoreBinaria<T> subArvore = new ArvoreBinaria<>();
         subArvore.raiz = no;
 
